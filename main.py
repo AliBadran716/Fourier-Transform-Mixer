23c4f0d9797218a3f326ab7778ac1cb17619e3f0
AliBadran716 <102072821+AliBadran716@users.noreply.github.com>
2023-12-07 20:59:56 +0200

    UI

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from PyQt5 import QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from PyQt5.QtGui import QPixmap
+
 from PyQt5.uic import loadUiType
 import pyqtgraph as pg
 
