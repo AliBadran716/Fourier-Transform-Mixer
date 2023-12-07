2bfb06ddc2579260961c9d3265e6e4b035e63e2c
Muhannad Abdallah <Muhannadd159@gmail.com>
2023-12-07 22:13:08 +0200

    Initialization code

--- image.py
+++ image.py
@@ -1,10 +1,34 @@
+import cv2
+import numpy as np
+from PyQt5.QtGui import QImage, QPixmap
 
+class Image:
+    def __init__(self, image_path):
+        self.image_path = image_path
+        self.image_data = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        self.compute_fourier_transform()
 
-class image_class:
-    def __init__(self):
-        # Initialization code for your background class
-        pass
+    def compute_fourier_transform(self):
+        f_transform = np.fft.fft2(self.image_data)
+        self.amplitudes = np.abs(f_transform)
+        self.phases = np.angle(f_transform)
 
-    def perform_background_task(self):
-        # Some functionality here
-        print("Performing background task")
+    def get_amplitudes(self):
+        return self.amplitudes
+
+    def get_phases(self):
+        return self.phases
+
+    def get_image_data(self):
+        return self.image_data
+
+    def apply_mixer(self, images_list, mix_ratio):
+        # Mix the amplitudes and phases of the images
+        mixed_amplitudes = mix_ratio * self.amplitudes + (1 - mix_ratio) * images_list.amplitudes
+        mixed_phases = mix_ratio * self.phases + (1 - mix_ratio) * images_list.phases
+
+        # Reconstruct the mixed image using the inverse Fourier transform
+        mixed_transform = mixed_amplitudes * np.exp(1j * mixed_phases)
+        mixed_image_data = np.fft.ifft2(mixed_transform).real
+
+        return mixed_image_data
--- main.py
+++ main.py
@@ -3,40 +3,67 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtGui import QPixmap
-
+from PyQt5.QtWidgets import QApplication, QFileDialog
 from PyQt5.uic import loadUiType
 import pyqtgraph as pg
 
 import numpy as np
 import pandas as pd
 
-from image import image_class
+from image import Image
 
 import os
 import sys
 from os import path
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QPushButton, QSlider
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QImage, QPixmap
+import sys
+from image import Image
 
 FORM_CLASS, _ = loadUiType(path.join(path.dirname(__file__), "main.ui"))  # connects the Ui file with the Python file
 
-
-class MainApp(QMainWindow, FORM_CLASS):  # go to the main window in the form_class file
+class MainApp(QMainWindow, FORM_CLASS): # go to the main window in the form_class file
 
     def __init__(self, parent=None):  # constructor to initiate the main window  in the design
         """
-                Constructor to initiate the main window in the design.
+                 Constructor to initiate the main window in the design.
 
-                Parameters:
-                - parent: The parent widget, which is typically None for the main window.
+                 Parameters:
+                 - parent: The parent widget, which is typically None for the main window.
         """
         super(MainApp, self).__init__(parent)
         self.setupUi(self)
+        self.images_list = []  # List to store Image instances
+        self.browsing_pushButton.clicked.connect(self.browse_image)
 
 
+    def browse_image(self):
+        # browse and get the image path as .jpg or .gif or .png or .jpeg or .svg
+        image_path, _ = QFileDialog.getOpenFileName(self, "Open Image", "", "Image Files (*.jpg *.gif *.png *.jpeg *.svg)")
+        self.image_instance = Image(str(image_path))
+        #self.display_image()
+        #print(image_path)
 
+    # def display_image(self):
+    #     if self.image_instance:
+    #         image_data = self.image_instance.get_image_data()
+    #         h, w = image_data.shape
+    #         bytes_per_line = w
+    #         q_image = QImage(image_data.data, w, h, bytes_per_line, QImage.Format_Grayscale8)
+    #         pixmap = QPixmap.fromImage(q_image)
+    #         self.image_label.setPixmap(pixmap)
 
-
-
-
+    # def mix_images(self):
+    #     # Implement logic to mix images using the slider value
+    #     if self.images_list:
+    #         mix_ratio = self.slider.value() / 100.0
+    #         mixed_image_data = self.image_instnace.apply_mixed_transform(self.images_list, mix_ratio)
+    #         h, w = mixed_image_data.shape
+    #         bytes_per_line = w
+    #         q_image = QImage(mixed_image_data.data, w, h, bytes_per_line, QImage.Format_Grayscale8)
+    #         pixmap = QPixmap.fromImage(q_image)
+    #         self.image_label.setPixmap(pixmap)
 
 def main():  # method to start app
     app = QApplication(sys.argv)
@@ -47,3 +74,12 @@ def main():  # method to start app
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
